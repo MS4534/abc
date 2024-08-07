8316b6b1d7b15ef8490e085ea657953fdd0b8069
Basava <159115403+MS4534@users.noreply.github.com>
2024-08-07 09:39:39 +0530

    Update nqueen.c

--- nqueen.c
+++ nqueen.c
@@ -1,67 +1,128 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#define True 1
-#define False 0
-int x[10],n,count;
+#include <stdio.h>
 
-void printsolution()
-{
-    char c[10][10];
-    int i,j;
-    printf("Solution %d: \n\n", ++count);
-    for(i=1; i<=n; i++)
-    {
-        for(j=1; j<=n; j++)
-        {
-            c[i][j]='X';
-        }
-    }
-    for(i=1; i<=n; i++)
-    {
-        c[i][x[i]]='Q';
-    }
-    for(i=1; i<=n; i++)
-    {
-        for(j=1; j<=n; j++)
-        {
-            printf("%c ", c[i][j]);
-        }
-        printf("\n");
-    }
+#include <stdbool.h>
+
+#define N 8 // You can change this to solve for different board sizes
+
+// Function to print the board
+
+void printBoard(int board[N][N]) {
+
+for (int i = 0; i < N; i++) {
+
+for (int j = 0; j < N; j++)
+
+printf(" %d ", board[i][j]);
+
+printf("\n");
+
 }
 
-int place(int k, int xk)
-{
-    for(int i=1; i<=k-1; i++)
-    {
-        if(x[i]==xk || abs(i-k) == abs(x[i]-xk))
-        {
-            return False;
-        }
-    }
-    return True;
 }
 
-void nqueen(int k)
-{
-    int j;
-    for(j=1; j<=n; j++)
-    {
-        if(place(k,j))
-        {
-            x[k]=j;
-            if(k==n)
-                printsolution(), printf("\n");
-            else
-                nqueen(k+1);
-        }
-    }
+// Function to check if a queen can be placed on board[row][col]
+
+bool isSafe(int board[N][N], int row, int col) {
+
+int i, j;
+
+// Check this row on left side
+
+for (i = 0; i < col; i++)
+
+if (board[row][i])
+
+return false;
+
+// Check upper diagonal on left side
+
+for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+
+if (board[i][j])
+
+return false;
+
+// Check lower diagonal on left side
+
+for (i = row, j = col; i < N && j >= 0; i++, j--)
+
+if (board[i][j])
+
+return false;
+
+return true;
+
 }
 
-void main()
-{
-    printf("Enter the number of queens\n");
-    scanf("%d", &n);
-    nqueen(1);
+// A recursive utility function to solve N Queen problem
+
+bool solveNQUtil(int board[N][N], int col) {
+
+// If all queens are placed, return true
+
+if (col >= N)
+
+return true;
+
+// Consider this column and try placing this queen in all rows one by one
+
+for (int i = 0; i < N; i++) {
+
+if (isSafe(board, i, col)) {
+
+// Place this queen in board[i][col]
+
+board[i][col] = 1;
+// Recur to place the rest of the queens
+
+if (solveNQUtil(board, col + 1))
+
+return true;
+
+// If placing queen in board[i][col] doesn't lead to a solution
+
+// then remove queen from board[i][col] (backtrack)
+
+board[i][col] = 0;
+
+}
+
+}
+
+// If the queen cannot be placed in any row in this column, return false
+
+return false;
+
+}
+
+// This function solves the N Queen problem using Backtracking
+
+// It mainly uses solveNQUtil() to solve the problem
+
+bool solveNQ() {
+
+int board[N][N] = {0};
+
+if (!solveNQUtil(board, 0)) {
+
+printf("Solution does not exist");
+
+return false;
+
+}
+
+printBoard(board);
+
+return true;
+
+}
+
+// Driver program to test the above functions
+
+int main() {
+
+solveNQ();
+
+return 0;
+
 }
